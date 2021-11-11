1325fc09029d888445d9bbfbffd4505e0baf1f50
jupyter <jupyter>
2021-11-11 16:17:00 +0000

    diagrams and new unet

--- include/architectures/unet.py
+++ include/architectures/unet.py
@@ -14,46 +14,9 @@ import tensorflow.keras.applications as A
 import loss
 
 '''
-REFERENCE
-first try
-has class imbalance
+VGG16
 '''
-def reference(name, input_height, input_width, number_classes, metrics = None):
-   # base_model = A.ResNet50V2(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3)) 
-    #base_model = A.InceptionV3(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3)) 
-    base_model = A.VGG16(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3))
-
-    pool = Conv2D(512, (1, 1))(base_model.output)
-    x = Conv2DTranspose(256, 3, strides=2, padding='same')(pool)
-    x = BatchNormalization()(x)
-    x = Activation("relu")(x)
-    for filters in [256,128, 64, 32]:
-        x = Conv2D(filters, 3, strides=1, padding='same')(x)
-        x = BatchNormalization()(x)
-        x = Activation("relu")(x)
-        x = Dropout(0.2)(x)
-        x = Conv2D(filters, 3, strides=1, padding='same')(x)
-        x = BatchNormalization()(x)
-        x = Activation("relu")(x)
-        x = Dropout(0.2)(x)
-        
-        x = Conv2DTranspose(filters, 3, strides=2, padding='same')(x)
-        x = BatchNormalization()(x)
-        x = Activation("relu")(x)
-        x = Dropout(0.2)(x)
-    output = Conv2D(number_classes, 1)(x)
-    output = Activation('softmax')(output)
-    
-    model = Model(inputs=base_model.inputs, outputs=output, name=name)
-    optimizer = Adam(lr=3e-4) # lr is learning rate
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=metrics) # mean squared error because it is a regression problem
-    #plot_model(model, to_file='%s.png' % (name))
-    return model
-
-'''
-REFERENCE_1
-'''
-def reference_1(name, input_height, input_width, number_classes, metrics = None):
+def vgg(name, input_height, input_width, number_classes, metrics = None):
     # base_model = A.ResNet50V2(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3)) 
     #base_model = A.InceptionV3(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3)) 
     base_model = A.VGG16(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3))
@@ -93,39 +56,35 @@ def reference_1(name, input_height, input_width, number_classes, metrics = None)
     #plot_model(model, to_file='%s.png' % (name))
     return model
 
-def tpu(name, input_height, input_width, number_classes, metrics = None):
-    # base_model = A.ResNet50V2(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3)) 
-    #base_model = A.InceptionV3(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3)) 
-    base_model = A.VGG16(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3))
-    # get end of blocks for skip connections
-    block5_output = base_model.get_layer('block5_conv3').output
-    block4_output = base_model.get_layer('block4_conv3').output
-    block3_output = base_model.get_layer('block3_conv3').output
-    block2_output = base_model.get_layer('block2_conv2').output
-    block1_output = base_model.get_layer('block1_conv2').output
-    blocks = [block5_output, block4_output, block3_output, block2_output, block1_output]
-
-    pool = Conv2D(1024, 3, strides=1, padding='same')(base_model.output)
-    x = Activation("relu")(pool)
+'''
+MobileNetV2
+'''
+def mobilenet(name, input_height, input_width, number_classes, metrics = None):
+    base_model = A.MobileNetV2(include_top=False, weights="imagenet", input_shape=(input_height,input_width,3))
     
-    # decoder
-    filters = [512,256,128,64,32]
-    for i,f in enumerate(filters):
-        x = Conv2D(f, 3, strides=1, padding='same')(x)
-        x = Activation("relu")(x)
-        x = UpSampling2D(interpolation='bilinear')(x)
+    skip_connection_names = ["input","block_1_expand_relu", "block_3_expand_relu", "block_6_expand_relu"]
+    encoder_output = base_model.get_layer("block_13_expand_relu").output
+    
+    filters = [16, 32, 48, 64]
+    x = encoder_output
+    for i in range(1, len(skip_connection_names)+1, 1):
+        if skip_connection_names[-i] == "input":
+            x_skip = base_model.input
+        else:
+            x_skip = base_model.get_layer(skip_connection_names[-i]).output
+        x = UpSampling2D(2, interpolation='bilinear')(x)
+        x = Concatenate()([x, x_skip])
         
-        x = Concatenate()([blocks[i], x])
-
-        x = Conv2D(f, 3, strides=1, padding='same')(x)
+        x = Conv2D(filters[-i], 3, strides=1, padding="same")(x)
+        x = BatchNormalization()(x)
         x = Activation("relu")(x)
-        x = Dropout(0.3)(x)
-        x = Conv2D(f, 3, strides=1, padding='same')(x)
+        
+        x = Conv2D(filters[-i], 3, strides=1, padding="same")(x)
+        x = BatchNormalization()(x)
         x = Activation("relu")(x)
-        x = Dropout(0.3)(x)
-    
-    output = Conv2D(number_classes, 1)(x)
-    output = Activation('softmax')(output)
+        
+    x = Conv2D(number_classes, 1, padding="same")(x)
+    output = Activation("softmax")(x)
     
     model = Model(inputs=base_model.inputs, outputs=output, name=name)
     optimizer = Adam(lr=3e-4) # lr is learning rate
